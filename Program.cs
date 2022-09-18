e02d8e0914000ce47b700a786e8dd2f9a57456b2
akos500 <akos500@yandex.ru>
2022-09-18 20:37:43 +0300

    Поиск наибольшего числа

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2. Написать программу, которая принимает
+// на вход 2 числа и выдает какое из них наибольшее,
+// а какое наименьшее.
+
+Console.WriteLine("Введите два числа : ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("{0} наибольшее число", a);
+}
+else
+{
+    Console.WriteLine("{0} наибольшее число ", b);
+}
+
